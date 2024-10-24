ee4299705f026df5809315d22bc939f4e9b2c865
unknown <joaquinx293@gmail.com>
2024-10-24 18:57:45 -0300

    Añadir el codigo del laboratorio_8 vercion 2

--- Desktop/Laboratorio_8.cpp
+++ Desktop/Laboratorio_8.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>  
-#include <chrono>   
-#include <iomanip>  
-#include <limits>   
+#include <cstdlib>
+#include <chrono>
+#include <iomanip>
+#include <limits>
+#include <algorithm>  
+#include <random>    
 
 using namespace std;
 using namespace chrono;
-void printVector(const vector<int>& vec) {
+
+void Mostrar_vector(const vector<int>& vec) {
     for (int i = 0; i < vec.size(); i++) {
         cout << "a[" << i << "]=" << vec[i] << " ";
     }
     cout << endl;
 }
-
-// aqui se implemntan los dos metodos
-void selectionSort(vector<int>& vec) {
+// aqui estan los distintos metodos de ordenamiento
+void Selecion_sort(vector<int>& vec) {
     int n = vec.size();
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
@@ -49,49 +51,47 @@ void quicksort(vector<int>& vec, int low, int high) {
     }
 }
 
-int main() {
+int Validar() {// valida que los valores que se entregran esten correctos
     int N;
-    char VER;
-
-    // Preguntar por el valor de N y lo valida
     do {
         cout << "Ingrese el numero de elementos a ordenar (N > 0): ";
         cin >> N;
-
-        // Validar si la entrada es un número válido y positivo
         if (cin.fail() || N <= 0) {
-            cout << "Error: Debe ingresar un número entero positivo.\n";
-            cin.clear();  // Limpiar el estado de error
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');  
+            cout << "Error: Debe ingresar un numero entero positivo.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
     } while (N <= 0);
+    return N;
+}
 
-    // Preguntar por el valor de VER y lo valida
+bool Preguntas() {
+    char VER;
     do {
         cout << "Desea mostrar el contenido del vector? (s/n): ";
         cin >> VER;
-
-        // Validar si la entrada es válida  's' o 'n'
         if (VER != 's' && VER != 'n') {
             cout << "Error: Debe ingresar 's' para mostrar o 'n' para no mostrar.\n";
         }
     } while (VER != 's' && VER != 'n');
+    return (VER == 's');
+}
 
-    bool mostrar = (VER == 's');  
+int main() {
+    int N = Validar();
+    bool mostrar = Preguntas();
 
-  
     vector<int> vec(N);
-    srand(time(0));  
-    for (int i = 0; i < N; i++) {
-        vec[i] = rand() % 10000;  
-    }
+    random_device rd;
+    mt19937 g(rd());
+    iota(vec.begin(), vec.end(), 0);
+    shuffle(vec.begin(), vec.end(), g);
 
     vector<int> vecSelection = vec;
     vector<int> vecQuicksort = vec;
 
-
     auto start = high_resolution_clock::now();
-    selectionSort(vecSelection);
+    Selecion_sort(vecSelection);
     auto end = high_resolution_clock::now();
     auto durationSelection = duration_cast<milliseconds>(end - start).count();
 
@@ -100,17 +100,15 @@ int main() {
     end = high_resolution_clock::now();
     auto durationQuicksort = duration_cast<milliseconds>(end - start).count();
 
-    // Aqui se muestra los resultado 
-    if (mostrar) {
+    if (mostrar) {// muestras el tiempo y el metodo 
         cout << "Contenido del vector ordenado (Selection Sort):\n";
-        printVector(vecSelection);
+        Mostrar_vector(vecSelection);
         cout << "Contenido del vector ordenado (Quicksort):\n";
-        printVector(vecQuicksort);
+        Mostrar_vector(vecQuicksort);
     }
-
 
     cout << "Seleccion\t|" << durationSelection << " milisegundos\n";
     cout << "Quicksort\t|" << durationQuicksort << " milisegundos\n";
-
     return 0;
 }
+
